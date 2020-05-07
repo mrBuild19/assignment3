b62d42ff3e9457bfec4e5e3be37d2f77d13d81c5
阿武大輔 <daisuke@MacBook-Air.local>
2020-05-07 23:21:36 +0900

    [Finish]assignment4

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   	resource :favorites, only: [:create, :destroy]
   	resources :book_comments, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
   root 'home#top'
   get 'home/about'
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  get 'follow_index/:id' => 'relationships#follow_index', as: 'follow_index'
+  get 'follower_index/:id' => 'relationships#follower_index', as: 'follower_index'
 end
